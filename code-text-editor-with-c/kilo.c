00098186a4948f4e33c2f2ade6e1fa85f9292c10
confucianzuoyuan <zuoyuantc@126.com>
2020-01-06 22:34:57 +0800

    fix

--- code-text-editor-with-c/kilo.c
+++ code-text-editor-with-c/kilo.c
@@ -313,8 +313,7 @@ void editorOpen(char *filename) {
 
   char *line = NULL;
   size_t linecap = 0;
-  ssize_t linelen = 0;
-  linelen = getline(&line, &linecap, fp);
+  ssize_t linelen;
 
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
